a3394c2644ba4f5a39e16f33c34c870c1b1c3323
prabhu3397 <74896218+prabhu3397@users.noreply.github.com>
2021-11-16 13:14:48 +0800

    Add files via upload

---
+++ lit.py
@@ -0,0 +1,78 @@
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
+from sklearn.model_selection import train_test_split
+import xgboost as xg
+
+
+
+
+
+st.title('House Price Predictor')
+st.write('DISCLAIMER')
+st.write('This tool is for educational purposes only')
+st.sidebar.header('House Features')
+image = Image.open('house.jpg')
+st.image(image, '')
+
+dataset = pd.read_csv("House data final.csv")
+
+# Function
+def user_report ():
+    bedrooms = st.sidebar.slider('bedroom',1,20,1,key=1)
+    bathrooms = st.sidebar.slider('bathroom', 1,20,1,key=2)
+    sqft_living = st.sidebar.slider('sqft living', 200,20000,200,key=3)
+    sqft_lot = st.sidebar.slider('sqft lot',200,20000,200,key=4 )
+    floors = st.sidebar.slider('floor',1,10,1)
+    waterfront = st.sidebar.slider('Index for waterfront',0,1,0,key=5)
+    view = st.sidebar.slider('Index for view',1,5,1,key=6)
+    condition = st.sidebar.slider('Index for condition',1,5,1,key=7)
+    grade = st.sidebar.slider('index for grade', 1,12,1,key=8)
+    sqft_above = st.sidebar.slider('sqft above', 100,10000,100,key=9)
+    sqft_basement = st.sidebar.slider('sqft basement', 100,20000,100,key=10)
+    yr_built = st.sidebar.slider('Year built', 1800,2021,1800,key=11)
+    yr_renovated = st.sidebar.slider('Year renovated', 0,2021,0,key=12)
+    
+    
+    
+    user_report_data = {
+        'bedrooms': bedrooms,
+            'bathrooms': bathrooms,
+            'sqft living': sqft_living,
+            'sqft lot': sqft_lot,
+            'floors': floors,
+            'waterfront': waterfront,
+            'view': view,
+            'condition': condition,
+            'grade': grade,
+            'sqft above': sqft_above,
+            'sqft basement': sqft_basement,
+            'year built': yr_built,
+            'year renovated': yr_renovated,
+            
+
+         }
+    
+    features = pd.DataFrame(user_report_data, index=[1])
+    return features
+
+X = dataset.iloc[:,1:].values
+y = dataset.iloc[:,0].values
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=0)
+
+
+base_model = xg.XGBRegressor()
+base_model.fit(X_train,y_train)
+
+model= base_model
+user_data = user_report()
+st.header('House price prediction')
+st.write(user_data)
+
+
+salary = model.predict(user_data)
+st.subheader('House Price is')
+st.subheader('$'+str(np.round(salary[0], 2)))
+
